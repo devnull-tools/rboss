558e2f536c1a1674ac3089f54253d5dd9b82e3af
Ataxexe <ataxexe@gmail.com>
2011-05-06 11:51:33 -0400

    More test methods

--- test/jboss_deploy_folder_test.rb
+++ test/jboss_deploy_folder_test.rb
@@ -4,15 +4,62 @@ require_relative 'test_helper'
 class DeployFolderTest < Test::Unit::TestCase
 
   def test_folder_inside_deploy
-    with :eap, "5.1" do |profile|
-      profile.add :deploy_folder, 'rboss-deploy'
-      profile.add :deploy_folder, 'rboss/deploy'
+    for_test do |profile|
+      profile.add :deploy_folder, 'deploy/rboss-deploy'
+      profile.add :deploy_folder, 'deploy/rboss/deploy'
+
+      for_assertions do
+        assert(File.exists? "#{@jboss.profile}/deploy/rboss-deploy")
+        assert(File.exists? "#{@jboss.profile}/deploy/rboss/deploy")
+      end
     end
 
-    for_assertions do
-      assert(File.exists? "#{@jboss.profile}/deploy/rboss-deploy")
-      assert(File.exists? "#{@jboss.profile}/deploy/rboss/deploy")
+    do_test_with :org, "5.1.0.GA"
+    do_test_with :org, "6.0.0.Final"
+    do_test_with :eap, "5.1"
+    do_test_with :eap, "5.0"
+    do_test_with :soa_p, 5
+    do_test_with :soa_p, "5.0.0"
+  end
+
+  def test_folder_outside_deploy
+    for_test do |profile|
+      profile.add :deploy_folder, 'rboss-deploy'
+      profile.add :deploy_folder, 'rboss/deploy'
+
+      for_assertions do
+        assert(File.exists? "#{@jboss.profile}/rboss-deploy")
+        assert(File.exists? "#{@jboss.profile}/rboss/deploy")
+      end
     end
+
+    do_test_with :org, "5.1.0.GA"
+    do_test_with :org, "6.0.0.Final"
+    do_test_with :eap, "5.1"
+    do_test_with :eap, "5.0"
+    do_test_with :soa_p, 5
+    do_test_with :soa_p, "5.0.0"
+  end
+
+  def test_folder_outside_jboss
+    for_test do |profile|
+      profile.add :deploy_folder, '/tmp/rboss-deploy'
+      profile.add :deploy_folder, '/tmp/rboss/deploy'
+
+      for_assertions do
+        assert(File.exists? "#{@jboss.profile}/rboss-deploy")
+        assert(File.exists? "#{@jboss.profile}/rboss/deploy")
+      end
+    end
+
+    do_test_with :org, "5.1.0.GA"
+    do_test_with :org, "6.0.0.Final"
+    do_test_with :eap, "5.1"
+    do_test_with :eap, "5.0"
+    do_test_with :soa_p, 5
+    do_test_with :soa_p, "5.0.0"
+
+    `rm -rf /tmp/rboss*`
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,15 @@ module TestHelper
     ENV["JBOSS_DIR"] or File.expand_path "~/jboss"
   end
 
-  def with type, version, base_profile = :all
+  def for_test &block
+    @test_block = block
+  end
+
+  def do_test_with type, version, base_profile = :all
+    with type, version, base_profile, &@test_block
+  end
+
+  def with type, version, base_profile
     map ||= {
       :eap => "#{jboss_dir}/eap/jboss-eap-#{version}/jboss-as",
       :soa_p => "#{jboss_dir}/soa-p/jboss-soa-p-#{version}/jboss-as",
@@ -17,15 +25,15 @@ module TestHelper
     }
 
     @jboss_profile = Profile::new map[type],
-                                  :type => :eap,
+                                  :type => type,
                                   :base_profile => base_profile,
                                   :profile => :rboss
     @jboss = @jboss_profile.jboss
     yield @jboss_profile
-    @jboss_profile.create
   end
 
   def for_assertions
+    @jboss_profile.create
     yield
     @jboss_profile.remove
   end
