05b5ffe4cf7ee623cb8ed35fa3a76c74078ff734
Ataxexe <ataxexe@gmail.com>
2011-05-07 13:50:11 -0300

    Tests for org 6 and portal 5.1

--- test/jboss_deploy_folder_test.rb
+++ test/jboss_deploy_folder_test.rb
@@ -1,12 +1,35 @@
+#                         The MIT License
+#
+# Copyright (c) 2011 Marcelo Guimarães <ataxexe@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 require_relative 'test_helper'
 
 class DeployFolderTest < Test::Unit::TestCase
 
   def setup
     @all= {
-      :org => "5.1.0.GA",
-      :eap => ["5.0", "5.1"],
-      :soa_p => ["5", "5.0.0"]
+      :org   => ["5.1.0.GA", "6.0.0.Final"],
+      :eap   => ["5.0",      "5.1"],
+      :soa_p => ["5",        "5.0.0"],
+      :epp   =>  "5.1"
     }
   end
 
@@ -38,9 +61,8 @@ class DeployFolderTest < Test::Unit::TestCase
       assert(File.exists? "#{jboss.profile}/rboss/deploy")
       assert(File.directory? "#{jboss.profile}/rboss/deploy")
 
-      #TODO check vfs.xml
       check_profile_xml "${jboss.server.home.url}rboss-deploy", "${jboss.server.home.url}rboss/deploy"
-      check_vfs_xml "${jboss.server.home.url}rboss-deploy", "${jboss.server.home.url}rboss/deploy"
+      check_vfs_xml "${jboss.server.home.url}rboss-deploy", "${jboss.server.home.url}rboss/deploy" unless jboss.version == "6.0.0.Final"
     end
 
     do_test
@@ -58,9 +80,8 @@ class DeployFolderTest < Test::Unit::TestCase
       assert(File.exists? "/tmp/rboss/deploy")
       assert(File.directory? "/tmp/rboss/deploy")
 
-      #TODO check vfs.xml
       check_profile_xml 'file:///tmp/rboss-deploy', 'file:///tmp/rboss/deploy'
-      check_vfs_xml 'file:///tmp/rboss-deploy', 'file:///tmp/rboss/deploy'
+      check_vfs_xml 'file:///tmp/rboss-deploy', 'file:///tmp/rboss/deploy' unless jboss.version == "6.0.0.Final"
     end
 
     do_test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,25 @@
+#                         The MIT License
+#
+# Copyright (c) 2011 Marcelo Guimarães <ataxexe@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 require 'test/unit'
 require 'logger'
 require 'rexml/document'
@@ -48,14 +70,16 @@ module TestHelper
 
   def do_test_with type, version, blocks
     map ||= {
-      :eap => "#{jboss_dir}/eap/jboss-eap-#{version}/jboss-as",
+      :eap   => "#{jboss_dir}/eap/jboss-eap-#{version}/jboss-as",
+      :epp   => "#{jboss_dir}/epp/jboss-epp-#{version}/jboss-as",
       :soa_p => "#{jboss_dir}/soa-p/jboss-soa-p-#{version}/jboss-as",
-      :org => "#{jboss_dir}/org/jboss-#{version}"
+      :org   => "#{jboss_dir}/org/jboss-#{version}"
     }
     @logger = Logger::new STDOUT
     @logger.level = Logger::INFO
     @jboss_profile = Profile::new map[type],
                                   :type => type,
+                                  :version => version,
                                   :base_profile => :all,
                                   :profile => :rboss,
                                   :logger => @logger
