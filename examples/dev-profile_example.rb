3e1749f1875aa168f5ce49d6a057e2b828464fa2
Ataxexe <ataxexe@gmail.com>
2011-07-01 11:01:19 -0400

    example

---
+++ examples/dev-profile_example.rb
@@ -0,0 +1,18 @@
+require_relative '../src/rboss'
+
+include JBoss
+
+profile = Profile::new "#{ENV["HOME"]}/jboss/org/jboss-5.1",
+                       :type => :org,
+                       :version => 5.1,
+                       :base_profile => :default,
+                       :profile => :dev
+
+profile.configure :jmx, :password => "admin"
+
+profile.add :deploy_folder, 'deploy/datasources'
+profile.add :deploy_folder, 'deploy/apps'
+
+profile.configure :run_conf, :heap_size => '1024m', :perm_size => '512m', :debug => :socket
+
+profile.create
